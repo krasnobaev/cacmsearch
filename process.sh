3cba270edbb835e61f0e9419425f76ac79cdf533
krasnobaev <alekseykrasnobaev@gmail.com>
2014-02-11 04:50:03 +0400

    allmost all rewrite; checking input; reformatting; etc.

--- process.sh
+++ process.sh
@@ -1,95 +1,259 @@
 #!/bin/bash
+#===============================================================================
+# FILE : process.sh
+# USAGE : ./process.sh
+#
+# DESCRIPTION : ---
+#
+# OPTIONS :---
+# REQUIREMENTS :---
+# AUTHOR : Aleksey Krasnobaev (https://github.com/krasnobaev)
+# COMPANY : ---
+# VERSION : 0.1 / 02/10/2014 10:38:53 PM MSK
+# LICENSE : 
+#===============================================================================
 
-## System/Software Requirements/Dependencies
+# Globals
 
-https://lucene.apache.org/core/4_0_0/demo/overview-summary.html#Setting_your_CLASSPATH
-http://stackoverflow.com/questions/9329650/java-classpath-linux
-.bashrc:
-$ echo $CLASSPATH
-/opt/lucene/core/lucene-core-4.6.1.jar:/opt/lucene/queryparser/lucene-queryparser-4.6.1.jar:/opt/lucene/demo/lucene-demo-4.6.1.jar:/opt/lucene/analysis/common/lucene-analyzers-common-4.6.1.jar
-export CLASSPATH
-JAVA_HOME=/usr/lib/jvm/default-java/
-export JAVA_HOME
+# Corpora
+CACM1DIR='ftp://ftp.cs.cornell.edu/pub/smart/cacm/'
+CACM2='http://ir.dcs.gla.ac.uk/resources/test_collections/cacm/cacm.tar.gz'
+CACM2DIR='http://ir.dcs.gla.ac.uk/resources/test_collections/cacm/'
+CACM3='http://dg3rtljvitrle.cloudfront.net/cacm.tar.gz'
+CACM3DIR='http://www.search-engines-book.com/collections/'
 
-# Query preparation:
-# 1. extract questions
-grep -Pzo "^[.]W(\n[^.][[:print:]]*)*" < ~/data/cacm/ir.dcs.gla.ac.uk/query.text | \
-# 2. get rid of unnecessary line feeds
-tr '\n' ' ' | \
-# 3. split one query per line
-awk '{ gsub(".W ", "\n") ; print $0 }' | \
-# 4. remove double spaces and save results
-awk '{ gsub(/^ */,"",$1) ; print $0 }' > ~/git/cacmsearch/queries.txt
+# input
+CACM_CORPUS='/usr/data/cacm/search-engines-book.com/cacm.html/'
+CACM_RAWQUERIES='/usr/data/cacm/ir.dcs.gla.ac.uk/query.text'
+CACM_REL='cacm.rel'
+CACM_QUERIES=''
 
-# 1. Indexing
-## Corpora: ~/data/cacm/search-engines-book.com/cacm.html/
-$ java org.apache.lucene.demo.IndexFiles -docs ~/data/cacm/search-engines-book.com/cacm.html/ -index ~/data/index/lucene/cacm2/
+# mediate folder/files
+LUCENE_INDEX='/usr/data/index/lucene/cacm2/'
 
-# 2. Query construstion
-## 2.1. extract questions
-grep -Pzo "^[.]W(\n[^.][[:print:]]*)*" < ~/data/cacm/ir.dcs.gla.ac.uk/query.text | \
-## 2.2. get rid of unnecessary line feeds
-tr '\n' ' ' | \
-## 2.3. split one query per line
-awk '{ gsub(".W ", "\n") ; print $0 }' | \
-## 2.4. remove double spaces and save results
-awk '{ gsub(/^ */,"",$1) ; print $0 }' > queries.txt
-## 2.5. removing some sysntactical parcing errors by hand
-gvim cacm.queries > cacm.queries
+QUERIES='queries.txt'
+SERP_PREFIX='cacm.lucene.serp'
+MAIN_SERP='serp'
+POSTINGS='postings'
 
-# 3. Retrieval running / SERP catching
-## SERP 10
-java org.apache.lucene.demo.SearchFiles -index ~/data/index/lucene/cacm2/index -queries queries2.txt -paging 10 > cacm.lucene.serp.top10
-## SERP 100
-java org.apache.lucene.demo.SearchFiles -index ~/data/index/lucene/cacm2/index -queries queries2.txt -paging 100 > cacm.lucene.serp.top100
-## SERP all
-java org.apache.lucene.demo.SearchFiles -index ~/data/index/lucene/cacm2/index -queries queries2.txt -paging 4000 > cacm.lucene.serp
+# output folder/files
+# WORKFOLDER='~/git/cacmsearch'
+METRICS='metrics'
 
-# 4. Rel preparations
-## 4.1. SERP refine
-$ awk 'BEGIN {cnt=0;}
-	$1 ~ /Searching/ {cnt++;}
-	$1 !~ /Searching/ && $2 !~ /total/ {
-		gsub(". /usr/data/CACM/search-engines-book.com/cacm.html/", "; ");
-		gsub(".html", "; ");
-		print cnt"; "$0
-	}' < cacm.lucene.serp.top10 > cacm.lucene.serp.top10
-$ awk 'BEGIN {cnt=0;}
-	$1 ~ /Searching/ {cnt++;}
-	$1 !~ /Searching/ && $2 !~ /total/ {
-		gsub(". /usr/data/CACM/search-engines-book.com/cacm.html/", "; ");
-		gsub(".html", "; ");
-		print cnt"; "$0
-	}' < cacm.lucene.serp.top100 > cacm.lucene.serp.top100
-$ awk 'BEGIN {cnt=0;}
-	$1 ~ /Searching/ {cnt++;}
-	$1 !~ /Searching/ && $2 !~ /total/ {
-		gsub(". /usr/data/CACM/search-engines-book.com/cacm.html/", "; ");
-		gsub(".html", "; ");
-		print cnt"; "$0
-	}' < cacm.lucene.serp > cacm.lucene.serp
+#===  FUNCTION  ================================================================
+# NAME : clean
+# DESCRIPTION : cleaning generated files
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function clean {
+	read -p 'Clean index? ' -n 1 -r; printf '\n'
+	if [[ -e $LUCENE_INDEX && ($REPLY =~ ^[Yy]$) ]]
+	then
+		rm $LUCENE_INDEX/*
+	fi
+	read -p "Clean SERPs? " -n 1 -r; printf '\n'
+	if [[ -e $SERP_PREFIX && ($REPLY =~ ^[Yy]$) ]]
+	then
+		rm $SERP_PREFIX*
+	fi
+	read -p "Clean metrics? " -n 1 -r; printf '\n'
+	if [[ -e $METRICS && ($REPLY =~ ^[Yy]$) ]]
+	then
+		rm $METRICS
+	fi
+}
 
-$ tr ';' ' ' < cacm.lucene.serp.refined | awk 'BEGIN {cnt=0;} $1 ~ cnt {printf substr($3,6,4) " "} $1 > cnt {cnt++;print '\n'}' > cacm.lucene.serp.postings
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : System/Software Requirements/Dependencies
+#        https://lucene.apache.org/core/4_0_0/demo/overview-summary.html#Setting_your_CLASSPATH
+#        http://stackoverflow.com/questions/9329650/java-classpath-linux
+#        .bashrc
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f00 {
+#	JAVA_HOME=/usr/lib/jvm/default-java/
+#	export JAVA_HOME	echo 'Current CLASSPATH: ' $CLASSPATH
+	CLASSPATH=$CLASSPATH:/usr/local/bin/lucene/core/lucene-core-4.6.1.jar
+	CLASSPATH=$CLASSPATH:/usr/local/bin/lucene/queryparser/lucene-queryparser-4.6.1.jar
+	CLASSPATH=$CLASSPATH:/usr/local/bin/lucene/demo/lucene-demo-4.6.1.jar
+	CLASSPATH=$CLASSPATH:/usr/local/bin/lucene/analysis/common/lucene-analyzers-common-4.6.1.jar
+	JAVA_HOME=/usr/lib/jvm/default-java/
+	echo "Following files must be contained in $CLASSPATH:"
+	echo '   lucene/core/lucene-core-4.6.1.jar'
+	echo '   lucene/queryparser/lucene-queryparser-4.6.1.jar'
+	echo '   lucene/demo/lucene-demo-4.6.1.jar'
+	echo '   lucene/analysis/common/lucene-analyzers-common-4.6.1.jar'
+#	export CLASSPATH
+}
 
-# 5. Results/metrics calculation
-## 1. calc Relevant items
-$ awk 'BEGIN {cnt=0;}
-	$1 ~ /Searching/ {cnt++;}
-	$1 !~ /Searching/ && $2 !~ /total/ {
-		gsub(". /usr/data/CACM/search-engines-book.com/cacm.html/", "; ");
-		gsub(".html", "; ");
-		print cnt"; "$0
-	}' < cacm.rel > base_metrics.txt
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Query preparation
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f0 {
+	# 1. extract questions
+	grep -Pzo "^[.]W(\n[^.][[:print:]]*)*" < $CACM_RAWQUERIES | \
+		# 2. get rid of unnecessary line feeds
+	tr '\n' ' ' | \
+		# 3. split one query per line
+	awk '{ gsub(".W ", "\n") ; print $0 }' | \
+		# 4. remove double spaces and save results
+	awk '{ gsub(/^ */,"",$1) ; print $0 }' > queries.txt
+}
 
-## 2. 
-$ awk 'BEGIN {cnt=0;}
-	$1 ~ /Searching/ {cnt++;}
-	$1 !~ /Searching/ && $2 !~ /total/ {
-		gsub(". /usr/data/CACM/search-engines-book.com/cacm.html/", "; ");
-		gsub(".html", "; ");
-		print cnt"; "$0
-	}' < serp_top10.txt &> base_metrics.txt
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Indexing
+# PARAMETERS : 
+# RETURNS  : 
+#===============================================================================
+function f1 {
+	java org.apache.lucene.demo.IndexFiles -docs $CACM_CORPUS -index $LUCENE_INDEX
+}
 
-# 6. Results comparing
-$ 
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Query construstion
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f2 {
+	## 2.1. extract questions
+	grep -Pzo "^[.]W(\n[^.][[:print:]]*)*" < $CACM_RAWQUERIES | \
+	
+	## 2.2. get rid of unnecessary line feeds
+	tr '\n' ' ' | \
+	
+	## 2.3. split one query per line
+	awk '{ gsub(".W ", "\n") ; print $0 }' | \
+	
+	## 2.4. remove double spaces and save results
+	awk '{ gsub(/^ */,"",$1) ; print $0 }' > queries.txt
+	
+	## 2.5. removing some syntactical errors
+	awk '{ gsub("", "") ; print $0 }' cacm.queries
+}
+
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Retrieval running / SERP catching
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f3 {
+	PARAMS='-index '$LUCENE_INDEX' -queries queries.txt'
+	## SERP 10
+	java org.apache.lucene.demo.SearchFiles $PARAMS -paging 10 > $SERP_PREFIX.top10
+	## SERP 100
+	java org.apache.lucene.demo.SearchFiles $PARAMS -paging 100 > $SERP_PREFIX.top100
+	## SERP all
+	java org.apache.lucene.demo.SearchFiles $PARAMS -paging 4000 > $SERP_PREFIX
+}
+
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Rel preparations
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f4 {
+	## 4.1. SERP refine
+	awk 'BEGIN {cnt=0;}
+		$1 ~ /Searching/ {cnt++;}
+		$1 !~ /Searching/ && $2 !~ /total/ {
+			gsub(". $CACM_CORPUS", "; ");
+			gsub(".html", "; ");
+			print cnt"; "$0}' < $SERP_PREFIX.top10 > $SERP_PREFIX.top102
+
+	awk 'BEGIN {cnt=0;}
+		$1 ~ /Searching/ {cnt++;}
+		$1 !~ /Searching/ && $2 !~ /total/ {
+			gsub(". $CACM_CORPUS", "; ");
+			gsub(".html", "; ");
+			print cnt"; "$0}' < $SERP_PREFIX.top100 > $SERP_PREFIX.top1002
+
+	awk 'BEGIN {cnt=0;}
+		$1 ~ /Searching/ {cnt++;}
+		$1 !~ /Searching/ && $2 !~ /total/ {
+			gsub(". $CACM_CORPUS", "; ");
+			gsub(".html", "; ");
+			print cnt"; "$0}' < $SERP_PREFIX > "$SERP_PREFIX"2
+
+	tr ';' ' ' < $SERP_PREFIX.refined | \
+		awk 'BEGIN {cnt=0;}
+			$1 ~ cnt {printf substr($3,6,4) " "}
+			$1 > cnt {cnt++;print "\n"}' > $SERP_PREFIX.postings
+}
+
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Results/metrics calculation
+# PARAMETERS : 
+# RETURNS : 
+#===============================================================================
+function f5 {
+	## 1. calc Relevant items
+	awk 'BEGIN {cnt=0;}
+		$1 ~ /Searching/ {cnt++;}
+		$1 !~ /Searching/ && $2 !~ /total/ {
+			gsub(". $CACM_CORPUS", "; ");
+			gsub(".html", "; ");
+			print cnt"; "$0}' < $CACM_REL > $METRICS
+	## 2. 
+	awk 'BEGIN {cnt=0;}
+		$1 ~ /Searching/ {cnt++;}
+		$1 !~ /Searching/ && $2 !~ /total/ {
+			gsub(". $CACM_CORPUS", "; ");
+			gsub(".html", "; ");
+			print cnt"; "$0}' < $SERP_PREFIX.top10 &> $METRICS
+}
+
+#===  FUNCTION  ================================================================
+# NAME : 
+# DESCRIPTION : Results Comparing
+# PARAMETERS : 
+# RETURNS : 
+#==============================================================================
+function f6 {
+	true;
+}
+
+#-------------------------------------------------------------------------------
+# main()
+#-------------------------------------------------------------------------------
+while getopts ":c" o; do
+	case "$o" in
+	c)
+		echo "Cleaning out . . ." >&2
+		clean
+		exit 0
+		;;
+	\?)
+		echo "Invalid option: -$OPTARG" >&2
+		exit 1
+		;;
+	esac
+done
+
+f0
+
+if [[ -e $LUCENE_INDEX/segments.gen ]]
+then
+	echo "Index exists, skipping indexing"
+else
+	echo 'Indexing from: ' $CACM_CORPUS ' to: ' $LUCENE_INDEX
+	f1
+fi
+f2
+echo test
+f3
+echo testagain
+f4
+f5
+f6
 
